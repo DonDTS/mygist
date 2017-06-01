ebdc20dd29fcc483ee4b4d3584ab5cc9d3ccb2fc
DonDTS <draks07@mail.ru>
2017-06-01 18:14:27 +0300

    Update common1.js

--- common1.js
+++ common1.js
@@ -60,6 +60,37 @@ $(document).ready(function() {
 	$("input, select, textarea").jqBootstrapValidation();
 
 	$(".top_mnu ul a").mPageScroll2id();
+	
+	    //Аякс отправка форм
+    //Документация: http://api.jquery.com/jquery.ajax/
+    $("#form").submit(function() {
+      $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: $(this).serialize()
+      }).done(function() {
+        alert("Спасибо за заявку!");
+        setTimeout(function() {
+          $.magnificPopup.close();
+          $("#form").trigger("reset");
+        }, 1000);
+      });
+      return false;
+    });
+
+    //Chrome Smooth Scroll
+    try {
+      $.browserSelector();
+      if ($("html").hasClass("chrome")) {
+        $.smoothScroll();
+      }
+    } catch (err) {
+
+    };
+
+    $("img, a").on("dragstart", function(event) {
+      event.preventDefault();
+    });
 
 });
 $(window).load(function() {
